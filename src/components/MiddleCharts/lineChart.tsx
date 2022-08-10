840f4ee1d7dc904c51cbed0170f9fa95d2687a9c
Ventura08 <estevaotevu08@gmail.com>
2022-08-10 00:56:14 -0300

    Modelo Básico de Chart pronto

---
+++ src/components/MiddleCharts/lineChart.tsx
@@ -0,0 +1,82 @@
+import react, { PureComponent } from 'react';
+import styled from 'styled-components';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const data = [
+  {
+    name: 'Segunda',
+    Faltas: 2400,
+  },
+  {
+    name: 'Terça',
+    Faltas: 1398,
+  },
+  {
+    name: 'Quarta',
+    Faltas: 1398,
+  },
+  {
+    name: 'Quinta',
+    Faltas: 1398,
+  },
+  {
+    name: 'Sexta',
+    Faltas: 1398,
+  },
+  {
+    name: 'Sabado',
+    Faltas: 1398,
+  },
+];
+
+type DynamicLineChartProps = {
+  className?: string;
+};
+
+const DynamicLineChart = ({ className }: DynamicLineChartProps) => {
+  return (
+    <div className={className}>
+      <ResponsiveContainer className="chart-container" width="50%" height="100%">
+        <BarChart
+          className="bar-chart"
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 30,
+            right: 30,
+            left: 5,
+            bottom: 5,
+          }}
+          
+        >
+        
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="Faltas" fill="#8884d8" radius={[5,5,0,0]} barSize={24}/>
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default styled(DynamicLineChart)`
+  display: flex;
+  justify-content: space-evenly;
+  height: 30%;
+  margin-left: 2%;
+  .chart-container {
+    width: 100%;
+    height: 100%;
+    margin: 2%;
+  }
+
+  .bar-chart {
+    border: 1px solid rgba(0, 0, 0, 0.18);
+    box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 13px;
+  }
+`;
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { HeaderApp } from './components/Header/header';
 import { Dashboard } from './pages/Dashboard/dashboard';
 import { ActiveRoom } from './pages/Active-Room/active-room';
+import DynamicLineChart from './components/MiddleCharts/lineChart';
 import { Login } from './pages/Login/login';
 
 export function AppRoutes() {
@@ -12,6 +13,7 @@ export function AppRoutes() {
           <Route path="/" element={<Dashboard/>} />
           <Route path="/index" element={<ActiveRoom/>} />
           <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="/middle-chart" element={<DynamicLineChart />} />
         </Routes>
       </HeaderApp>
       {/* <Routes>
