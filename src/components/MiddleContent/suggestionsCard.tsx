06856b894f74a9ee40a57662be4625ab27a54658
Ventura08 <estevaotevu08@gmail.com>
2022-08-20 20:10:01 -0300

    .

--- src/components/MiddleContent/suggestionsCard.tsx
+++ src/components/MiddleContent/suggestionsCard.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
-
 type DynamicSuggestionsCardProps = {
   className?: string;
   children: any;
@@ -11,7 +9,7 @@ type DynamicSuggestionsCardProps = {
 const DynamicSuggestionsCard = ({ className, children }: DynamicSuggestionsCardProps) => {
   return (
     <div className={`${className} col-md-5 col-sm-12`}>
-<div className="title-div">Justificativa de faltas pendentes</div>
+      <div className="title-div">Justificativa de faltas pendentes</div>
       {children}
     </div>
   );
@@ -24,7 +22,11 @@ export default styled(DynamicSuggestionsCard)`
   border: 1px solid rgba(0, 0, 0, 0.18);
   box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 13px;
-  overflow: auto;
+  background: #fafafa;
+  overflow-y: auto;
+  ::-webkit-scrollbar {
+    display: none;
+  }
   .title-div {
     min-width: 100%;
     margin-left: 4%;
